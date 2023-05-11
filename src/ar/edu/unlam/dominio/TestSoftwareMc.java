701497066cac2e5cca6571db5d4df7390a78d3fe
Termine Gaston <gastontermine16@gmail.com>
2023-05-11 19:57:55 -0300

    claseTestCreada

---
+++ src/ar/edu/unlam/dominio/TestSoftwareMc.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSoftwareMc {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
