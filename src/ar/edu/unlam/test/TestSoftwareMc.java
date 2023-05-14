61ed2b0822747605940e76722e138eb4122d2ada
maurocoldes <maurocoldes@gmail.com>
2023-05-14 16:24:30 -0300

    SegundoTestCompleto

--- src/ar/edu/unlam/test/TestSoftwareMc.java
+++ src/ar/edu/unlam/test/TestSoftwareMc.java
@@ -265,7 +265,21 @@ public class TestSoftwareMc {
 	}
 	//////////////////////////////////
 	
-	
+	@Test
+	public void queElDeMantenimientoPuedaArreglarUnPuestoDeTrabajo() {
+		//INICIALIZACION
+		SoftwarePrincipal sf=new SoftwarePrincipal("SISTEMA DE MACDONALS 2023");
+		Mantenimiento empleadoDeMantenimiento=new Mantenimiento(003,"JORGE","LOPEZ",15890240,1200.0);
+		Cocina cocina=new Cocina(EstadoDelPuesto.EN_REPARACION,4,1,false);
+		
+		//EJECUCION
+		Boolean evaluarAAgregarPuestoDeTrabajo = empleadoDeMantenimiento.agregarUnaCocina(cocina, sf);
+		empleadoDeMantenimiento.arreglarPuestoDeTrabajo(cocina);
+		
+		//EVALUACION
+		assertEquals(EstadoDelPuesto.SEGURO, cocina.getEstadoDelPuesto());
+		
+	}
 	
 	
 }
